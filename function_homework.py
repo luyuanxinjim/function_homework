fc2e4d9cbc472420cff7f2df795fef1dc4aa3411
luyuanxinjim <29952240584@qq.com>
2018-06-30 20:50:04 +0800

    'hi'

---
+++ function_homework.py
@@ -0,0 +1,92 @@
+def get_income(hot_dog)
+   income=(6*hot_dog) - 3000
+   return income
+
+
+print income(400)
+print income(400)
+
+
+
+
+def foo(n)
+    num= (6 * n）- 4
+    return num
+
+
+
+print(foo(4))
+print(foo(5))
+print(foo(6)) 
+print(foo(7))
+
+
+
+
+def coo(a):
+	num = (1 / (3 ** a - 1))
+	return num
+
+
+print(coo(1))
+print(coo(2))
+print(coo(3))
+print(coo(4))
+print(coo(5))
+
+
+
+
+def func(c):
+	num = (-1 ** c) * (3)
+	return num
+
+
+
+print(func(1))
+print(func(2))
+print(func(3))
+print(func(4))
+print(func(5))
+
+
+
+def money(x):
+
+          num = 250 + (250 * 0.07) * x
+
+          return num
+
+
+
+
+
+print(money(1))
+
+print(money(3))
+
+print(money(7))
+
+print(money(20))
+
+
+
+
+
+def cool(a):
+
+         num = 325 + (325 * 0.04) * a
+
+         return num
+
+
+
+
+
+print(cool(1))
+
+print(cool(3))
+
+print(cool(7))
+
+print(cool(20))
